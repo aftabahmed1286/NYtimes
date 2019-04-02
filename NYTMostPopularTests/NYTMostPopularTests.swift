9a09813ec97ed4140778ab4b5a676fc04047d39e
Aftab Ahmed <aahmed03@in.ibm.com>
2019-04-02 17:49:43 +0530

    Initial Commit

---
+++ NYTMostPopularTests/NYTMostPopularTests.swift
@@ -0,0 +1,34 @@
+//
+//  NYTMostPopularTests.swift
+//  NYTMostPopularTests
+//
+//  Created by Aftab Ahmed on 4/2/19.
+//  Copyright © 2019 FAMCO. All rights reserved.
+//
+
+import XCTest
+@testable import NYTMostPopular
+
+class NYTMostPopularTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
